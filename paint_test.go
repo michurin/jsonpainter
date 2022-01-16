a850deb9cc8962ca6c88c403c4fb4905de6489e4
Alexey Michurin <a.michurin@gmail.com>
2022-01-16 09:14:09 +0300

    Testing: black-box mode, examples

--- paint_test.go
+++ paint_test.go
@@ -1,6 +1,11 @@
-package jsonpainter
+package jsonpainter_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/michurin/jsonpainter"
+)
 
 func TestString(t *testing.T) {
 	cases := []struct {
@@ -44,24 +49,24 @@ func TestString(t *testing.T) {
 		in:   `[]]`,
 		exp:  `(C][[O)(C]][O)]`,
 	}}
-	optsColors := []Option{
-		ClrKey([]byte("(Q]")),
-		ClrStr([]byte("(s]")),
-		ClrSpecStr([]byte("(S]")),
-		ClrCtl([]byte("(C]")),
-		ClrOff([]byte("[O)")),
+	optsColors := []jsonpainter.Option{
+		jsonpainter.ClrKey([]byte("(Q]")),
+		jsonpainter.ClrStr([]byte("(s]")),
+		jsonpainter.ClrSpecStr([]byte("(S]")),
+		jsonpainter.ClrCtl([]byte("(C]")),
+		jsonpainter.ClrOff([]byte("[O)")),
 	}
-	optsNoColors := []Option{
-		ClrKey(nil),
-		ClrStr(nil),
-		ClrSpecStr(nil),
-		ClrCtl(nil),
-		ClrOff(nil),
+	optsNoColors := []jsonpainter.Option{
+		jsonpainter.ClrKey(nil),
+		jsonpainter.ClrStr(nil),
+		jsonpainter.ClrSpecStr(nil),
+		jsonpainter.ClrCtl(nil),
+		jsonpainter.ClrOff(nil),
 	}
 	for _, c := range cases {
 		c := c
 		t.Run("color_"+c.name, func(t *testing.T) {
-			o := String(c.in, optsColors...)
+			o := jsonpainter.String(c.in, optsColors...)
 			if o != c.exp {
 				t.Errorf("%s != %s", o, c.exp)
 			}
@@ -70,10 +75,31 @@ func TestString(t *testing.T) {
 	for _, c := range cases {
 		c := c
 		t.Run("no_color_"+c.name, func(t *testing.T) {
-			o := String(c.in, optsNoColors...)
+			o := jsonpainter.String(c.in, optsNoColors...)
 			if o != c.in {
 				t.Errorf("%s != %s", o, c.in)
 			}
 		})
 	}
+}
+
+func ExampleString_simplest() {
+	// Example of log line with mixture JSON and non-JSON parts
+	logline := `Req: {"rc": 1}`
+	// As far as go examples doesn't support escape control sequences, it has to be used %q here.
+	// In real application this line might look like fmt.Println(jsonpainter.String(logline))
+	fmt.Printf("%q", jsonpainter.String(logline))
+	// Output: "Req: \x1b[31;1m{\x1b[0m\x1b[33;1m\"rc\"\x1b[0m\x1b[31;1m:\x1b[0m \x1b[36;1m1\x1b[0m\x1b[31;1m}\x1b[0m"
+}
+
+func ExampleString_withOptions() {
+	opts := []jsonpainter.Option{
+		jsonpainter.ClrCtl(nil),              // We don't want to colorize {, }, [, ], :, and comma
+		jsonpainter.ClrKey(jsonpainter.Cyan), // Keys will be cyan
+		jsonpainter.ClrSpecStr(nil),          // Special values (true, false, null...) won't be painted
+		jsonpainter.ClrStr(nil),              // Strings won't be painted as well
+	}
+	logline := `Req: {"rc": 1}`
+	fmt.Printf("%q", jsonpainter.String(logline, opts...))
+	// Output: "Req: {\x1b[36;1m\"rc\"\x1b[0m: 1}"
 }
